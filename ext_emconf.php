d715a46a790f43ab96743cbec333852558ac8158
Andre Kraus <info@andrekraus.com>
2023-04-07 17:46:26 +0200

    [VERSION] update version number

--- ext_emconf.php
+++ ext_emconf.php
@@ -27,5 +27,5 @@ $EM_CONF['wsm_form_spamshield'] = [
     'author' => 'Andre Kraus',
     'author_email' => 'info@website-mensch.de',
     'author_company' => 'Website Mensch',
-    'version' => '1.1.0',
+    'version' => '1.2.0',
 ];
