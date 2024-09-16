4c08e31d7d6cdc18fd4f3bb4f68dd63d0b398b90
Andre Kraus <info@andrekraus.com>
2024-09-16 17:24:33 +0200

    [FIX] add scope to javascript variables fixes #13

--- Resources/Public/JavaScript/Dist/form-security-check.js
+++ Resources/Public/JavaScript/Dist/form-security-check.js
@@ -1,65 +1,70 @@
-let securityTests = {};
-let form = document.querySelector('form');
+(function() {
 
-/* Count seconds, stop at 90 seconds */
-let seconds = 0;
-let secondsIntervall = setInterval(incrementSeconds, 1000);
-function incrementSeconds() {
-    seconds += 1;
-    securityTests['seconds'] = seconds;
-    if (seconds === 2) {
-        setDefaultValues();
+    let securityTests = {};
+    let form = document.querySelector('form');
+
+    /* Count seconds, stop at 90 seconds */
+    let seconds = 0;
+    let secondsIntervall = setInterval(incrementSeconds, 1000);
+
+    function incrementSeconds() {
+        seconds += 1;
+        securityTests['seconds'] = seconds;
+        if (seconds === 2) {
+            setDefaultValues();
+        }
+        setValue();
+        // stop seconds counting at 90 seconds
+        if (seconds >= 90) {
+            clearInterval(secondsIntervall);
+        }
     }
-    setValue();
-    // stop seconds counting at 90 seconds
-    if (seconds >= 90) {
-        clearInterval(secondsIntervall);
+
+    let securityFieldList = Array.from(document.querySelectorAll('form input.security-check-input'));
+
+    /** set default values after 2 seconds. */
+    function setDefaultValues() {
+        /* get some default values */
+        securityTests['displayWidth'] = screen.width;
+        securityTests['displayHeight'] = screen.height;
+        securityTests['formRenderedHeight'] = form.offsetHeight;
+        securityTests['formRenderedWidth'] = form.offsetWidth;
     }
-}
 
-let securityFieldList = Array.from(document.querySelectorAll('form input.security-check-input'));
+    /* Scroll */
+    document.addEventListener('scroll', (event) => {
+        securityTests['scroll'] = ((securityTests['scroll'] !== undefined) ? securityTests['scroll'] + 1 : 1);
+        setValue();
+    });
 
-/** set default values after 2 seconds. */
-function setDefaultValues() {
-    /* get some default values */
-    securityTests['displayWidth'] = screen.width;
-    securityTests['displayHeight'] = screen.height;
-    securityTests['formRenderedHeight'] = form.offsetHeight;
-    securityTests['formRenderedWidth'] = form.offsetWidth;
-}
+    /* MouseMove */
+    document.addEventListener('mousemove', (event) => {
+        securityTests['mousemove'] = ((securityTests['mousemove'] !== undefined) ? securityTests['mousemove'] + 1 : 1);
+        setValue();
+    });
 
-/* Scroll */
-document.addEventListener('scroll', (event) => {
-    securityTests['scroll'] = ((securityTests['scroll'] !== undefined) ? securityTests['scroll'] + 1 : 1);
-    setValue();
-});
+    /* MouseClick */
+    document.addEventListener('click', (event) => {
+        securityTests['mouseClickX'] = event.screenX;
+        securityTests['mouseClickY'] = event.screenY;
+        setValue();
+    });
 
-/* MouseMove */
-document.addEventListener('mousemove', (event) => {
-    securityTests['mousemove'] = ((securityTests['mousemove'] !== undefined) ? securityTests['mousemove'] + 1 : 1);
-    setValue();
-});
+    /* Any key press */
+    document.addEventListener('keypress', (event) => {
+        if (event.key === "@") securityTests['pressedAT'] = 1;
+        if (event.key === " ") securityTests['pressedWhiteSpace'] = 1;
+        securityTests['keypress'] = ((securityTests['keypress'] !== undefined) ? securityTests['keypress'] + 1 : 1);
+        setValue();
+    });
 
-/* MouseClick */
-document.addEventListener('click', (event) => {
-    securityTests['mouseClickX'] = event.screenX;
-    securityTests['mouseClickY'] = event.screenY;
-    setValue();
-});
-
-/* Any key press */
-document.addEventListener('keypress', (event) => {
-    if(event.key === "@") securityTests['pressedAT'] = 1;
-    if(event.key === " ") securityTests['pressedWhiteSpace'] = 1;
-    securityTests['keypress'] = ((securityTests['keypress'] !== undefined) ? securityTests['keypress'] + 1 : 1);
-    setValue();
-});
-
-/* Sets security values to the hidden field. */
-function setValue() {
-    if (Array.isArray(securityFieldList)){
-        securityFieldList.forEach(function(securityField, index, arr){
-            securityField.value = JSON.stringify(securityTests);
-        });
+    /* Sets security values to the hidden field. */
+    function setValue() {
+        if (Array.isArray(securityFieldList)) {
+            securityFieldList.forEach(function (securityField, index, arr) {
+                securityField.value = JSON.stringify(securityTests);
+            });
+        }
     }
-}
+
+})();
