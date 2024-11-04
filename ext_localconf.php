28b7c94a39cd58381d8e8786bf1f895642aa61a8
Andre Kraus <info@andrekraus.com>
2024-11-04 11:11:18 +0100

    [FIX] always load TypoScript even in site sets in TYPO3 v13.4, thx to @linawolf in #14 #15

--- ext_localconf.php
+++ ext_localconf.php
@@ -11,4 +11,38 @@ call_user_func(static function () {
     );
 
     $GLOBALS['TYPO3_CONF_VARS']['SC_OPTIONS']['ext/form']['afterSubmit'][1680904711] = \WebsiteMensch\FormSpamshield\Hooks\AfterSubmitHook::class;
+
+    \TYPO3\CMS\Core\Utility\ExtensionManagementUtility::addTypoScriptSetup(
+        trim('
+            ########################
+            #### FORM FRAMEWORK ####
+            ########################
+
+            # Register custom EXT:form configuration
+            module.tx_form {
+                settings {
+                    yamlConfigurations {
+                        1678469420 = EXT:wsm_form_spamshield/Configuration/Yaml/EditorBaseSetup.yaml
+                    }
+                }
+                view {
+                    partialRootPaths {
+                        1678469420 = EXT:wsm_form_spamshield/Resources/Private/Frontend/Partials/Forms/
+                    }
+                }
+            }
+            plugin.tx_form {
+                settings {
+                    yamlConfigurations {
+                        1678469420 = EXT:wsm_form_spamshield/Configuration/Yaml/BaseSetup.yaml
+                    }
+                }
+                view {
+                    partialRootPaths {
+                        1678469420 = EXT:wsm_form_spamshield/Resources/Private/Frontend/Partials/Forms/
+                    }
+                }
+            }
+        ')
+    );
 });
