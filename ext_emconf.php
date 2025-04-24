828ab784681d01626ef236b218a21dc079efceb1
Andre Kraus <info@andrekraus.com>
2025-04-24 15:57:57 +0200

    [RELEASE] v1.5.0: - [FEATURE] add some more security checks

--- ext_emconf.php
+++ ext_emconf.php
@@ -27,5 +27,5 @@ $EM_CONF['wsm_form_spamshield'] = [
     'author' => 'Andre Kraus',
     'author_email' => 'info@website-mensch.de',
     'author_company' => 'Website Mensch',
-    'version' => '1.4.1',
+    'version' => '1.5.0',
 ];
